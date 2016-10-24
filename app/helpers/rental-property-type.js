4bcc7221b76324f10b41ea81d2724d9b902d26ea
Brett New <brettnew@Bretts-MacBook-Air.local>
2016-10-23 19:31:59 -0700

    Add functionality to allow user to filter properties by city.

--- app/helpers/rental-property-type.js
+++ app/helpers/rental-property-type.js
@@ -7,7 +7,7 @@ const communityPropertyTypes = [
 ];
 
 export function rentalPropertyType([type]/*, hash*/) {
-  if (communityPropertyTypes.includes(type)){
+  if (communityPropertyTypes.includes(type)) {
     return 'Community';
   }
   return 'Standalone';
